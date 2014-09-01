5d137793a708d0caef78d6a7d8c5689374154de8
Frank Ammeter <frank.ammeter@bsiag.com>
2014-09-01 07:54:14 +0200

    added floating point arithmetic example

---
+++ java/FloatArithmetic.java
@@ -0,0 +1,9 @@
+public class FloatArithmetic {
+    public static void main(String[] args) {
+        double tenth = 1.0/10;
+        double threeTenths = tenth + tenth + tenth;
+        System.out.println(threeTenths == 0.3);
+        System.out.println(threeTenths - 0.3);
+    }
+}
+
