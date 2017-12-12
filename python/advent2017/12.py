fe50aac4da3e9d1383b0224d523bfb92a45cbce8
Frank Ammeter <frank.ammeter@bsi-software.com>
2017-12-12 22:33:51 +0100

    beautified

--- python/advent2017/12.py
+++ python/advent2017/12.py
@@ -3,12 +3,10 @@ import fileinput
 from collections import defaultdict
 
 pipe = defaultdict(list)
-unreachables = set()
 
 def find_group(root):
     todo = { root }
     reachable = { root }
-    global unreachables
 
     while len(todo) > 0:
         new_todo = set()
@@ -20,20 +18,21 @@ def find_group(root):
         todo = new_todo
 
     print(root, ': ', len(reachable))
+    global unreachables
     unreachables = unreachables.difference(reachable)
     return len(reachable)
-
 
 for line in fileinput.input():
     m = re.search('(\\d+) <-> (.+)', line)
     if m:
         pl, pr_str = m.groups()
         pr = re.split(',\\s*', pr_str)
-        unreachables.add(int(pl))
         for r in pr:
             pipe[int(pl)].append(int(r))
     else:
         print(line, ': no match')
+
+unreachables = set(pipe.keys())
 
 totsize = 0
 ngroups = 0
