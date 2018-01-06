d7fd778fff1e7a7466cfc5a187a0df68fcad4677
Frank Ammeter <frank.ammeter@bsi-software.com>
2018-01-06 18:06:47 +0100

    24 working

--- python/advent2017/24.py
+++ python/advent2017/24.py
@@ -3,34 +3,42 @@ import re
 
 l = []
 maxweight = 0
+maxlength = 0
 
 def printsol(li):
     w = 0
+    length = 0
     for i,v in enumerate(li):
         it = l[v[0]]
-        print(i, it, v[1])
+#        print(i, it, v[1])
         w += it[0] + it[1]
-    print('weight ', w)
-    return w
+        length += 1
+#    print('weight ', w)
+    return (w,length)
 
 def search(li, si):
     global maxweight
-    w = printsol(li)
-    if w > maxweight:
-        maxweight = w
-        print('maxweight is ', maxweight)
+    global maxlength
 
     last = li[-1]
     lastv = l[last[0]][1-last[1]]
-    print('lastv is ', lastv)
+    found = False
     for i,v in enumerate(l):
         if i not in si:
             if v[0] == lastv or v[1] == lastv:
+                found = True
                 mi = int(v[1] == lastv)
                 si.add(i)
                 li.append([i,mi])
-                search(li, si)
-
+                if not search(li, si):
+                    w,length = printsol(li)
+                    if length > maxlength or (length == maxlength and w > maxweight):
+                        maxlength = length
+                        maxweight = w
+                        print('maxlength/weight is ', maxlength, maxweight)
+                si.remove(i)
+                li.pop()
+    return found
             
 
 for line in fileinput.input():
