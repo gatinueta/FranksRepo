71f3812d1dee71466855192aa484ccc53cebaddf
Frank Ammeter <frank.ammeter@bsi-software.com>
2018-05-26 22:34:13 +0200

    cleanup sieve

--- python/sieve.py
+++ python/sieve.py
@@ -1,13 +1,18 @@
-p = [True] * 101
+from math import sqrt
+MAX = 101
+p = [True] * MAX
 
-for i in range(2,10):
-    for j in range(i*2, 101, i):
-        p[j] = False
+for i in range(2,int(sqrt(MAX))+1):
+    if p[i]:
+        for j in range(i*2, MAX, i):
+            p[j] = False
+
+primes = [i for i in range(2,MAX) if p[i]]
+print(primes)
 
 res = 1
-for i in range(2,101):
+for i in range(2,MAX):
     if p[i]:
         res *= i
-        print(i)
 
 print(res) 
