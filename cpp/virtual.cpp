699b6aa4fd78c4bf110f940b33d89ac26e61def4
Frank Ammeter <git@ammeter.ch>
2019-05-06 22:00:16 +0200

    virtual

---
+++ cpp/virtual.cpp
@@ -0,0 +1,23 @@
+class A {
+public:
+	virtual void foo() = 0;
+	void bar();
+};
+
+class B: public A {
+	void foo();
+};
+
+int main(int argc, char** argv) {
+	B b;
+	A& a = b;
+	a.bar();
+	return 0;
+}
+
+void A::bar() {
+}
+
+void B::foo() {
+}
+
