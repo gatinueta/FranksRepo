a0bb8a17a81b14f0d9d71d1b2c2bf1f6cfe9671a
Frank Ammeter <frank.ammeter@bsi-software.com>
2019-05-19 15:01:11 +0200

    Zoes first program

---
+++ java/ZoesProgram.java
@@ -0,0 +1,12 @@
+public class ZoesProgram {
+    public static void main(String[] args) {
+        int zoesA  = 100;
+        zoesA -= 5;
+        System.out.println(zoesA);
+        zoesA+=4;                                             
+        System.out.println(zoesA);
+        zoesA-=10;                                   
+        System.out.println(zoesA);
+    }
+}
+
