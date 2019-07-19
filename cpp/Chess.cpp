3be656307e465787eb8067cb3899396ed9539820
Frank Ammeter <frank.ammeter@bsi-software.com>
2019-07-19 21:38:27 +0200

    chess

--- cpp/Chess.cpp
+++ cpp/Chess.cpp
@@ -6,7 +6,6 @@ class Piece {
    bool m_black;
 public:
    void setBlack(bool black) {
-     std::cout << "setting black to " << black << std::endl;
      m_black = black;
    }
    bool isBlack() const {
@@ -16,6 +15,9 @@ public:
    
    const char sym() const {
      return m_black ? getSym() : tolower(getSym());
+   }
+   virtual bool isKing() const {
+     return false;
    }
 };
 
@@ -37,6 +39,7 @@ public:
     const char getSym() const {
         return 'N';
     }
+
 };
 
 class Rook : public Piece {
@@ -63,29 +66,12 @@ class King : public Piece {
     const char getSym() const {
         return 'K';
     }
+    bool isKing() const {
+        return true;
+    }
 };
 
 
-class Field {
-    Piece *m_content;
-public:
-    Field() {
-        m_content = NULL;
-    }
-    const Piece *put(Piece& p) {
-        Piece *old = m_content;
-        m_content = &p;
-        return old;
-    }
-    const Piece *remove() {
-        Piece *old = m_content;
-        m_content = NULL;
-        return old;
-    }
-    const Piece* get() const {
-        return m_content;
-    }
-};
 const static int DIM = 8;
 
 const static std::string COLNAMES = "ABCDEFGH";
@@ -94,6 +80,8 @@ const static std::string ROWNAMES = "12345678";
 struct Point {
     int x, y;
     Point(int ax, int ay) : x(ax), y(ay) {
+    }
+    Point() : Point(0, 0) {
     }
     static Point convert(std::string pos) {
         size_t x = COLNAMES.find(pos[0]);
@@ -105,9 +93,44 @@ struct Point {
     }
 };  
 
+class Field {
+    Piece *m_content;
+public:
+    Point m_pos; // TODO not public
+    Field() {
+        m_content = nullptr;
+    }
+    const Piece *put(Piece& p) {
+        Piece *old = m_content;
+        m_content = &p;
+        return old;
+    }
+    const Piece *remove() {
+        Piece *old = m_content;
+        m_content = nullptr;
+        return old;
+    }
+    const Piece* get() const {
+        return m_content;
+    }
+};
+
 class Board {
     Field m_b[DIM * DIM];
+    bool m_whitesTurn;
+    Point m_bKingPos, m_wKingPos;
 public:
+    Board() : m_whitesTurn(true) {
+        Point p;
+        for (int y=0; y<DIM; y++) {
+            p.y = y;
+            for (int x=0; x<DIM; x++) {
+                p.x = x;
+                Field& f = get(p);
+                f.m_pos = p;
+            }
+        }
+    }
     Field& get(Point p) {
         return m_b[p.y + DIM*p.x];
     }
@@ -117,11 +140,29 @@ public:
     Field& get(const std::string& pos) {
         return this->get(Point::convert(pos));
     }
+    bool whitesTurn() {
+        return m_whitesTurn;
+    }
+    bool validate() {
+        int n_wKings = 0, n_bKings = 0;
+        for(const Field& field : m_b) {
+            if (field.get() && field.get()->isKing()) {
+                if (field.get()->isBlack()) {
+                    m_bKingPos = field.m_pos;
+                    n_bKings++;
+                 } else {
+                    m_wKingPos = field.m_pos;
+                    n_wKings++;
+                 }
+            }
+         }
+         return n_wKings == 1 && n_bKings == 1;
+    }
 };
 
 std::ostream& operator<<(std::ostream &strm, const Field &f) {
     const Piece *p = f.get();
-    strm << (p == NULL ? '.' : p->sym());
+    strm << (p == nullptr ? '.' : p->sym());
     return strm;
 }
 
@@ -169,12 +210,6 @@ static std::shared_ptr<T> add(Board& b, const char col, const char row, bool bla
 }
 
 static auto place_pieces(Board& b, const char row, const bool black) {
-    Rook w_r2;
-    Bishop w_b1, w_b2;
-    Knight w_n1, w_n2;
-    Queen w_q;
-    King w_k;
-    
     std::vector<P_Piece> ptrs;
     
     add<Rook>(b, 'A', row, black, ptrs);
@@ -196,6 +231,9 @@ int main(int argc, char **argv) {
     auto black_pawns = place_pawns(b, '7', true);
     auto white_pieces = place_pieces(b, '1', false);
     auto black_pieces = place_pieces(b, '8', true);
+    if (!b.validate()) {
+        std::cout << "error: validate failed" << std::endl;
+    }
     std::cout << b << std::endl;
     return 0;
 }
