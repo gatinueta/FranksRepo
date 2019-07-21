682926e104bf87b43929467ea1cab3f710afbcdb
Frank Ammeter <frank.ammeter@bsi-software.com>
2019-07-21 09:13:35 +0200

    knight

--- cpp/Chess.cpp
+++ cpp/Chess.cpp
@@ -48,18 +48,20 @@ std::ostream& operator<<(std::ostream &strm, const Point &p) {
     return strm;
 }
 
+class Field;
+
 class Piece {
    bool m_black;
 protected:
-   Point m_pos;
+   const Field* m_field;
 public:
-   Piece() : m_pos(Point::OFFBOARD) {
+   Piece() : m_field(nullptr) {
    }
-   void setPos(const Point& pos) {
-     m_pos = pos;
+   void setField(const Field *field) {
+     m_field = field;
    }
-   Point getPos() const {
-     return m_pos;
+   const Field *getField() const {
+     return m_field;
    }
    void setBlack(bool black) {
      m_black = black;
@@ -79,6 +81,11 @@ public:
      return false;
    }
    virtual std::vector<Point> getTargets() const { // TODO pure virtual?
+     return {};
+   }
+   std::vector<Point> getDirectTargets() const;
+
+   virtual const std::vector<Point> getIncs() const {
      return {};
    }
 
@@ -101,11 +108,22 @@ public:
 };
 
 class Knight : public Piece {
+    static const std::vector<Point> M_INCS;
 public:
     const char getSym() const {
         return 'N';
     }
+    const std::vector<Point> getIncs() const {
+        return M_INCS;
+    }
+    std::vector<Point> getTargets() const;
+};
 
+const std::vector<Point> Knight::M_INCS = { 
+    Point(1,-2), Point(1,2), 
+    Point(2,-1), Point(2,1), 
+    Point(-1,-2), Point(-1,2), 
+    Point(-2,-1), Point(-2,1)
 };
 
 class Rook : public Piece {
@@ -129,7 +147,7 @@ class Bishop : public Piece {
 };
 
 class King : public Piece {
-    const static Point M_INCS[];
+    const static std::vector<Point> M_INCS;
 public:
     const char getSym() const {
         return 'K';
@@ -137,34 +155,35 @@ public:
     bool isKing() const {
         return true;
     }
-
+    const std::vector<Point> getIncs() const {
+        return M_INCS;
+    }
     std::vector<Point> getTargets() const;
 };
 
-const Point King::M_INCS[] = { 
+const std::vector<Point> King::M_INCS = { 
     Point(1,-1), Point(1,0), Point(1,1), 
     Point(0,-1), Point(0,1), 
     Point(-1,-1), Point(-1,0), Point(-1,1) 
 };
 
-std::vector<Point> King::getTargets() const {
-        std::vector<Point> targets;
-        for (Point inc: M_INCS) {
-            Point target = m_pos + inc;
-            if (target.isOnBoard()) {
-                targets.push_back(target);
-            }
-        }
-        return targets;
-}
 
+class Board;
 
 class Field {
     Piece *m_content;
+    const Board *m_b;
+    Point m_pos;
 public:
-    Point m_pos; // TODO not public
+    void init(const Point pos, const Board *b) {
+        m_pos = pos;
+        m_b = b;
+    }
     Point getPos() const {
         return m_pos;
+    }
+    const Board* getBoard() const {
+        return m_b;
     }
     Field() {
         m_content = nullptr;
@@ -173,9 +192,9 @@ public:
         Piece *old = m_content;
         m_content = &p;
         if (old != nullptr) {
-            old->setPos(Point(-1, -1));
+            old->setField(nullptr);
         }
-        p.setPos(m_pos);
+        p.setField(this);
         return old;
     }
     const Piece *remove() {
@@ -186,7 +205,11 @@ public:
     const Piece* get() const {
         return m_content;
     }
+    bool isEmpty() const {
+        return m_content == nullptr;
+    }
 };
+
 
 class Board {
     Field m_b[DIM * DIM];
@@ -200,7 +223,7 @@ public:
             for (int x=0; x<DIM; x++) {
                 p.x = x;
                 Field& f = get(p);
-                f.m_pos = p;
+                f.init(p, this);
             }
         }
     }
@@ -228,10 +251,10 @@ public:
         for(const Field& field : m_b) {
             if (field.get() && field.get()->isKing()) {
                 if (field.get()->isBlack()) {
-                    m_bKingPos = field.m_pos;
+                    m_bKingPos = field.getPos();
                     n_bKings++;
                  } else {
-                    m_wKingPos = field.m_pos;
+                    m_wKingPos = field.getPos();
                     n_wKings++;
                  }
             }
@@ -252,6 +275,29 @@ public:
          return true;
     }
 };
+
+std::vector<Point> Piece::getDirectTargets() const {
+    std::vector<Point> targets;
+    const Board *b = m_field->getBoard();
+    for (Point inc: getIncs()) {
+        Point target = m_field->getPos() + inc;
+        if (target.isOnBoard()) {
+            if ( b->at(target).isEmpty() || b->at(target).get()->isBlack() != isBlack()) {
+                targets.push_back(target);
+            }
+        }
+    }
+    return targets;
+}
+
+std::vector<Point> King::getTargets() const {
+    return getDirectTargets();
+}
+
+std::vector<Point> Knight::getTargets() const {
+    return getDirectTargets();
+}
+
 
 std::ostream& operator<<(std::ostream &strm, const Field &f) {
     const Piece *p = f.get();
@@ -337,6 +383,9 @@ int main(int argc, char **argv) {
     const Piece* wKing = b.get(b.getKingPos(false)).get();
     auto targets = wKing->getTargets();
     std::cout << "white king targets: " << targets << std::endl;
+    const Piece* bKnight = b.get("B8").get();
+    targets = bKnight->getTargets();
+    std::cout << "black knight targets: " << targets << std::endl;
     return 0;
 }
 
