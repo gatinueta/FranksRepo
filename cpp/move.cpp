db3e5d6bca9abcae40e539ebab896632774cc2e4
Frank Ammeter <frank.ammeter@bsi-software.com>
2019-08-20 15:17:16 +0200

    more examples, corrected

--- cpp/move.cpp
+++ cpp/move.cpp
@@ -2,6 +2,10 @@
 #include <string>
 #include <iostream>
 
+static void debug(std::string msg) {
+    std::cerr << msg << std::endl;
+}
+
 struct M {
     int m_len;
     std::unique_ptr<char[]> m_p;
@@ -9,17 +13,20 @@ struct M {
         m_len(len), 
         m_p(std::make_unique<char[]>(len))
     {
+        debug("M(int) (create)");
     }
     M(const M& other) : 
         m_len(other.m_len), 
         m_p(std::make_unique<char[]>(m_len)) 
     {
-        memcpy(m_p.get(), other.m_p.get(), m_len);
+        debug("M(M&) (copy)");
+        std::copy(other.m_p.get(), other.m_p.get() + m_len, m_p.get());
     }
     ~M() {
         // noop, unique_ptr auto-destroyed
     }
     M& operator=(const M& other) {
+        debug("M =M& (copy assign)");
         if (&other != this) {
             m_len = other.m_len;
             m_p = std::make_unique<char[]>(m_len);
@@ -31,14 +38,15 @@ struct M {
         m_len(other.m_len),
         m_p(std::move(other.m_p)) 
       {
+        debug("M(M&&) (move)");
         other.m_len = 0;
         other.m_p.reset();
       }
     M& operator=(M&& other) {
+        debug("M =M&& (move assign)");
         if (&other != this) {
             m_len = other.m_len;
             m_p = std::move(other.m_p);
-            std::copy(other.m_p.get(), other.m_p.get() + m_len, m_p.get());
             other.m_p.reset();
         }
         return *this;
@@ -62,7 +70,15 @@ std::ostream& operator<<(std::ostream& str, const std::vector<T>& v) {
     }
     return str;
 }
-        
+
+M getM() {
+    M m(100);
+    m.set(0, 'e');
+    m.set(1, 'x');
+
+    return m;
+}
+
 int main() {
     std::vector<std::string> v = { "eins", "zwei", "drei", "vier" };
     std::vector<std::string> v2 = std::move(v);
@@ -70,15 +86,23 @@ int main() {
     std::cout << v << std::endl;
     std::cout << v2 << std::endl;
 
+    debug("make one");
     M m(10);
     m.set(0, 'a');
     m.set(1, 'b');
     std::cout << m << std::endl;
+    debug("copy it");
     M m2(m);
     m.set(1, '\0');
     std::cout << m << m2 << std::endl;
 
+    debug("moving second to third");
     M m3(std::move(m2));
+    debug("assigning second by value from function");
+    m2 = getM();
+    debug("assigning first a copy of the third");
+    m = m3;
+    debug("output everything");
     std::cout << m << m2 << m3 << std::endl;
 
 }
