e23fc10fb634e93c524fc39cf34dc2a8ba5246ea
Frank Ammeter <frank@ammeter.ch>
2022-11-22 21:34:41 +0100

    inner tricks

--- java/src/ch/frank/InnerExtended.java
+++ java/src/ch/frank/InnerExtended.java
@@ -3,20 +3,27 @@ package ch.frank;
 class InnerBase {
 	public class Inner {
 		public class InnerInner {
-			public void f() {
+			public class InnerInnerInner {
+				public void f() {}
 			}
 		}
 	}
 }
 
 public class InnerExtended extends InnerBase {
-	class InnerExtendedInner extends InnerBase.Inner.InnerInner {
-		public InnerExtendedInner(InnerBase.Inner inner) {
+	class InnerExtendedInner extends InnerBase.Inner.InnerInner.InnerInnerInner {
+		public InnerExtendedInner(InnerBase.Inner.InnerInner inner) {
 			inner.super();
 		}
 		@Override
 		public void f() {
 		}
+	}
+	public static void main(String[] args) {
+		InnerBase innerBase = new InnerBase();
+		InnerBase.Inner.InnerInner innerInner = innerBase.new Inner().new InnerInner();
+		InnerExtendedInner ei = new InnerExtended().new InnerExtendedInner(innerInner);
+		ei.f();
 	}
 }
 
